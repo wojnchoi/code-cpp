8811e7c2c77e1c0b82911b1bd99225a2f397d0cd
Woojin Choi <wjc0712h@gmail.com>
2021-02-06 11:30:46 -0800

    todo

--- sandbox/bitwise/bit.c
+++ sandbox/bitwise/bit.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 
+int getBit(int num, int i) {
+    return (num & (1<<i)) != 0;
+}
 int main()
 {
     unsigned int num1 = 7; // 0000 0111
@@ -9,5 +12,7 @@ int main()
     printf("%d\n", num1 | num2);
     printf("%d\n", num1 ^ num2);
     printf("%d\n", ~num1);
+
+    printf("%d\n",getBit(9,3));
     return 0;
 }
